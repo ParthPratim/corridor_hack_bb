9d63b166ba68ad9da63cedcebe0f5ce269214798
Aradhya kumar <aradhyasaumay21@gmail.com>
2023-03-25 17:34:08 +0530

    updates

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,21 +1,17 @@
 import { React, useState }from "react";
 import axios from "axios";
 import './Form.css'
-
+import image from "./assets/image.jpeg"
 const submitHandler =(event) => {
     event.preventDefault();
-    console.log(event)
     var doc_request = {
         "section_1" : {
             "devname" : event.target[0].value,
             "modelname"  : event.target[1].value,
             "overview": event.target[2].value
-            // docmument and find some common titles
         },
         "section_2": {
             "content" : event.target[3].value,
-            // "file" : document.getElementById("file").value,
-
         }
     }
 
@@ -39,8 +35,12 @@ const Form = () => {
 
     return (
         <div className="container-full">
+        <div style={{display:'flex'}}>
+        <div className="column-right">
+            <img src={image} width={700} height={700} alt="vector"/>
+        </div>
             <form className="login-form" method="post" action="http://127.0.0.1:6969/api/doctool/generate/" enctype="multipart/form-data" >
-            <h1>Submit Your Details</h1>
+            <h1>Generate your ML Document!</h1>
                 <div className="form-input-material">
                     <label htmlFor='devname'>Developer Name : </label>
                     <input type= "text" name = "devname" id="devname" autoComplete="off" className="form-control-material"
@@ -57,14 +57,14 @@ const Form = () => {
                 </div>
                 <div className="form-input-material">
                     <label htmlFor='modeloverview'> Model Overview : </label>
-                    <input type= "textarea" name ="overview" autoComplete="off" className="form-control-material"
-                    value={modeloverview} i
+                    <textarea rows={5} cols={15}  name ="overview" autoComplete="off" className="form-control-material"
+                    value={modeloverview} 
                     onChange ={(e) => setModeloverview(e.target?.value)}
                     />
                 </div>
                 <div className="form-input-material">
                     <label htmlFor='modelselection'>Reason for Model Selection : </label>
-                    <input type= "text" name = "content" id="modelselection" autoComplete="off" className="form-control-material"
+                    <textarea rows={5} cols={15} name = "content" id="modelselection" autoComplete="off" className="form-control-material"
                     value={modelselection}
                     onChange ={(e) => setModelselection(e.target?.value)}
                     />
@@ -78,6 +78,8 @@ const Form = () => {
                 </div>
                 <button type='submit' className="btn btn-primary btn-ghost">Generate PDF</button>
             </form>
+
+        </div>
         </div>
     )
 }
