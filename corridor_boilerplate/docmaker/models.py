e991d6b85aabf27e3e5397bf3cee10bdbc1e3619
Parth Pratim Chatterjee <parth27official@gmail.com>
2023-03-25 16:15:42 +0530

    Adjust size of data model variables

--- corridor_boilerplate/docmaker/models.py
+++ corridor_boilerplate/docmaker/models.py
@@ -4,8 +4,8 @@ class ModelDetails(models.Model):
     doc_id = models.BigAutoField(primary_key=True)
     devname = models.CharField(max_length=100)
     modelname = models.CharField(max_length=100)
-    overview = models.CharField(max_length=1000)
-    reason = models.CharField(max_length=1000)
+    overview = models.CharField(max_length=10000)
+    reason = models.CharField(max_length=10000)
     
 
     class Meta:
